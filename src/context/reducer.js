a6c35efd24fdb59eddd3b1a120876885f846d241
dilshod <dilshodx516@gmail.com>
2024-11-15 15:26:23 +0500

    NORNLIGHT

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,15 +1,21 @@
-export const inititalState = {
+export const inititalState = JSON.parse(localStorage.getItem("storage")) ||
+{
     wishlist: []
 }
 
 export const reducer = (state, action) => {
+    let result = state
     switch (action.type) {
         case "WISHLIST":
             let index = state.wishlist.findIndex(produc => produc.id === action.payload.id)
             if (index < 0) {
-                return { ...state, wishlist: [...state.wishlist, action.payload] }
+                result = { ...state, wishlist: [...state.wishlist, action.payload] }
+                localStorage.setItem("storage", JSON.stringify(result))
+                return result
             } else {
-                return { ...state, wishlist: state.wishlist.filter(produc => produc.id !== action.payload.id) }
+                result = { ...state, wishlist: state.wishlist.filter(produc => produc.id !== action.payload.id) }
+                localStorage.setItem("storage", JSON.stringify(result))
+                return result
             }
         default:
             return state
